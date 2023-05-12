43e45f027416fd7220fb8fe047e0981846011051
Ismael Herrera <ismaelherrera.dr@outlook.com>
2023-05-11 20:13:06 -0400

    Improve font styling for Mainheader and Headline

--- components/main-header.tsx
+++ components/main-header.tsx
@@ -1,7 +1,7 @@
 export default function Mainheader() {
   return (
     <section className="container  min-h-screen items-center gap-6 pb-8 pt-6 md:py-10">
-      <h1 className="headline text-center font-semibold">
+      <h1 className="headline text-center">
         Solución
         <br /> completa de <br />
         publicidad

2e304d42249f1148c9d5e766154bbfd1e0e6629e
Ismael Herrera <ismaelherrera.dr@outlook.com>
2023-05-14 19:11:55 -0400

    Add animated gradient headline to main header

--- components/main-header.tsx
+++ components/main-header.tsx
@@ -1,13 +1,9 @@
+import MainHeadline from './main-headline';
+
 export default function Mainheader() {
   return (
     <section className="container  min-h-screen items-center gap-6 pb-8 pt-6 md:py-10">
-      <h1 className="headline text-center">
-        Solución
-        <br /> completa de <br />
-        publicidad
-        <br />
-        digital
-      </h1>
+      <MainHeadline />
     </section>
   );
 }
---
+++ components/main-headline.tsx
@@ -0,0 +1,87 @@
+'use client';
+
+import { motion, useTime, useMotionValue, useMotionTemplate, useTransform } from 'framer-motion';
+import { useEffect } from 'react';
+
+export default function MainHeadline() {
+  const colors = useMotionValue((360 / 10000) * (360 / 10000) * Math.random());
+
+  return (
+    <>
+      <motion.div
+        className="gradientprovider_provider transform-gpu"
+        style={
+          {
+            '--gradient-progress': 0,
+            '--gradient-color-left': '#64b2ff',
+            '--gradient-color-center': '#8e84ff',
+            '--gradient-color-right': '#d499ed',
+            '--gradient': '#8e84ff, #d499ed, #f47b8f, #fabfb2, #fffad1, #95f3d9, #64b2ff, #8e84ff, #d499ed, #f47b8f',
+            '--gradient-interval-size': 3,
+            '--gradient-length': 10,
+            '--gradient-update-delay': 1000,
+          } as any
+        }
+        animate={
+          {
+            '--gradient-progress': [0, 1],
+            '--gradient-color-left': [
+              '#64b2ff',
+              '#ef81a1',
+              '#f47b8f',
+              '#fabfb2',
+              '#fffad1',
+              '#95f3d9',
+              '#64b2ff',
+              '#8e84ff',
+              '#d499ed',
+              '#f47b8f',
+              '#ef81a1',
+            ],
+            '--gradient-color-center': [
+              '#64b2ff',
+              '#ef81a1',
+              '#f47b8f',
+              '#fabfb2',
+              '#fffad1',
+              '#95f3d9',
+              '#64b2ff',
+              '#8e84ff',
+              '#d499ed',
+              '#f47b8f',
+              '#ef81a1',
+            ],
+            '--gradient-color-right': [
+              '#64b2ff',
+              '#ef81a1',
+              '#f47b8f',
+              '#fabfb2',
+              '#fffad1',
+              '#95f3d9',
+              '#64b2ff',
+              '#8e84ff',
+              '#d499ed',
+              '#f47b8f',
+              '#ef81a1',
+            ],
+          } as any
+        }
+        transition={{
+          duration: 10,
+          repeat: Infinity,
+          ease: 'linear',
+        }}
+      >
+        <section className="animate-mask-reveal transform-gpu">
+          <motion.h1 className="sectionhero_headline gradientprovider_diagonal mb-[0.2em] flex transform-gpu flex-col justify-start whitespace-nowrap bg-clip-text pb-[0.2em] text-center leading-[0.9] text-transparent [--font-scale:0.17] after:inline-block sm:[--font-scale:0.124] lg:[--font-scale:0.127]">
+            Solución
+            <br /> completa de <br />
+            publicidad
+            <br />
+            digital
+          </motion.h1>
+        </section>
+      </motion.div>
+    </>
+  );
+}

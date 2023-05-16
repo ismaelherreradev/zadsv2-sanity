20b234637e7aef69c014686a04522346b7e1b6b3
Ismael Herrera <ismaelherrera.dr@outlook.com>
2023-05-15 20:43:19 -0400

    update main-header

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Mainheader from '@/components/main-header';
 
 export default function IndexPage() {
   return (
-    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+    <section className="gap-6 pb-8 pt-6 md:py-10">
       <Mainheader />
     </section>
   );
--- components/main-header.tsx
+++ components/main-header.tsx
@@ -2,9 +2,9 @@ import MainHeadline from './main-headline';
 
 export default function Mainheader() {
   return (
-    <section className="animate-mask-reveal container min-h-screen transform-gpu items-center gap-6 pb-8 pt-6 md:py-10">
+    <section className="animate-mask-reveal container mx-auto min-h-screen transform-gpu  pb-8 pt-6 md:py-10">
       <MainHeadline />
-      <p className="mx-auto mt-8 px-[5%] text-center text-2xl font-normal leading-9 sm:mt-10 sm:max-w-md sm:px-0 md:mt-8 md:max-w-lg xl:max-w-xl">
+      <p className="mx-auto mt-8 px-[5%] text-center text-lg font-normal leading-9 sm:mt-10 sm:max-w-md sm:px-0 md:mt-8 md:max-w-lg md:text-2xl xl:max-w-xl">
         Aumentamos tus ventas con sistemas automatizados de publicidad digital en plataformas como{' '}
         <span className="font-semibold underline decoration-[#F4B400]">Google Ads</span>,{' '}
         <span className="font-semibold underline decoration-[#4285F4]">Meta Ads</span> y más...

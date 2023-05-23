c3cecaab2c03df111e6e77cdd87bf9198ac8980d
Ismael Herrera <ismaelherrera.dr@outlook.com>
2023-05-22 22:58:54 -0400

    Refactor section headings in Benefits and Why components

--- components/sections/benefits.tsx
+++ components/sections/benefits.tsx
@@ -1,13 +1,11 @@
 export default function Benefits() {
   return (
     <section className="container mx-auto py-24">
-      <div className="">
-        <div className="text-center">
-          <span className="font-mono uppercase tracking-[5px] text-z-yellow">Beneficios</span>
-          <h2 className="mx-auto font-serif text-4xl font-medium text-[#f7f7f8] md:max-w-xl md:px-0 md:text-5xl lg:max-w-2xl lg:text-6xl xl:max-w-3xl">
-            Brindamos soluciones avanzadas de publicidad digital.
-          </h2>
-        </div>
+      <div className="text-center">
+        <span className="font-mono uppercase tracking-[5px] text-z-yellow">Beneficios</span>
+        <h2 className="mx-auto font-serif text-4xl font-medium text-[#f7f7f8] md:max-w-xl md:px-0 md:text-5xl lg:max-w-2xl lg:text-6xl xl:max-w-3xl">
+          Brindamos soluciones avanzadas de publicidad digital.
+        </h2>
       </div>
     </section>
   );
--- components/sections/why.tsx
+++ components/sections/why.tsx
@@ -1,13 +1,11 @@
 export default function Why() {
   return (
     <section className="container mx-auto py-24">
-      <div className="">
-        <div className="text-center">
-          <span className="font-mono uppercase tracking-[5px] text-z-yellow">¿Por qué publicidad digital?</span>
-          <h2 className="mx-auto font-serif text-4xl font-medium text-[#f7f7f8] md:max-w-xl md:px-0 md:text-5xl lg:max-w-4xl lg:text-6xl xl:max-w-5xl">
-            Todos tus productos y servicios en múltiples plataformas automáticamente.
-          </h2>
-        </div>
+      <div className="text-center">
+        <span className="font-mono uppercase tracking-[5px] text-z-yellow">¿Por qué publicidad digital?</span>
+        <h2 className="mx-auto font-serif text-4xl font-medium text-[#f7f7f8] md:max-w-xl md:px-0 md:text-5xl lg:max-w-4xl lg:text-6xl xl:max-w-5xl">
+          Todos tus productos y servicios en múltiples plataformas automáticamente.
+        </h2>
       </div>
     </section>
   );

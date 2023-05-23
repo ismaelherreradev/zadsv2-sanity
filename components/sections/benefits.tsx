a9100e27f5ecbc1bd2f5ed551c83e2f2b6b8a195
Ismael Herrera <ismaelherrera.dr@outlook.com>
2023-05-22 22:46:13 -0400

    Adjust font sizes in benefits and why sections

--- components/sections/benefits.tsx
+++ components/sections/benefits.tsx
@@ -4,7 +4,7 @@ export default function Benefits() {
       <div className="">
         <div className="text-center">
           <span className="font-mono uppercase tracking-[5px] text-z-yellow">Beneficios</span>
-          <h2 className="mx-auto max-w-md font-serif text-7xl font-medium text-[#f7f7f8] sm:max-w-xl md:px-0 lg:max-w-3xl xl:max-w-4xl">
+          <h2 className="mx-auto font-serif text-4xl font-medium text-[#f7f7f8] md:max-w-xl md:px-0 md:text-5xl lg:max-w-2xl lg:text-6xl xl:max-w-3xl">
             Brindamos soluciones avanzadas de publicidad digital.
           </h2>
         </div>
--- components/sections/why.tsx
+++ components/sections/why.tsx
@@ -4,7 +4,7 @@ export default function Why() {
       <div className="">
         <div className="text-center">
           <span className="font-mono uppercase tracking-[5px] text-z-yellow">¿Por qué publicidad digital?</span>
-          <h2 className="mx-auto max-w-5xl font-serif text-7xl font-medium text-[#f7f7f8]  md:px-0 ">
+          <h2 className="mx-auto font-serif text-4xl font-medium text-[#f7f7f8] md:max-w-xl md:px-0 md:text-5xl lg:max-w-4xl lg:text-6xl xl:max-w-5xl">
             Todos tus productos y servicios en múltiples plataformas automáticamente.
           </h2>
         </div>

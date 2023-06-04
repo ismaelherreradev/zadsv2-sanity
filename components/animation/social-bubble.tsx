0fbd3ef032ff4144c96e62e5fd89c56bab0b2344
Ismael Herrera <ismaelherrera.dr@outlook.com>
2023-06-04 19:24:17 -0400

    Add social media bubbles animation and styles

--- components/animation/social-bubble.tsx
+++ components/animation/social-bubble.tsx
@@ -1,36 +1,61 @@
+'use client';
+
+import { motion } from 'framer-motion';
 import { Icons } from '../icons';
 
 export default function SocialBubbleAnimation() {
   return (
     <div className="relative aspect-video lg:mx-0 lg:aspect-square">
       <div className="relative flex h-full flex-col overflow-y-hidden">
-        <div className="z-10 flex min-w-full flex-[0_0_auto] flex-col items-center">
-          <div className="bubble_leftRight group relative -left-4 flex aspect-square w-20 items-center justify-center overflow-hidden rounded-full border after:absolute after:inset-0 after:rounded-full sm:w-24 md:w-32">
+        <motion.div
+          className="z-10 flex min-w-full flex-[0_0_auto] flex-col items-center"
+          initial={{ y: 700 }}
+          animate={{ y: -510 }}
+          transition={{ repeat: Infinity, duration: 30 }}
+        >
+          <div className="bubble_leftRight3 bubble-circle">
             <Icons.facebook className="h-12 w-12" />
+            <div className="bg-noisy pointer-events-none absolute inset-0 z-10 rounded-xl md:rounded-2xl lg:rounded-3xl" />
           </div>
-          <div className="bubble_leftRight2 group relative -left-4 flex aspect-square w-20 items-center justify-center overflow-hidden rounded-full border after:absolute after:inset-0 after:rounded-full sm:w-24 md:w-32">
+          <div className="bubble_leftRight4 bubble-circle">
             <Icons.twitter className="h-12 w-12" />
+            <div className="bg-noisy pointer-events-none absolute inset-0 z-10 rounded-xl md:rounded-2xl lg:rounded-3xl" />
           </div>
-          <div className="bubble_leftRight3 group relative -left-4 flex aspect-square w-20 items-center justify-center overflow-hidden rounded-full border after:absolute after:inset-0 after:rounded-full sm:w-24 md:w-32">
+          <div className="bubble_leftRight3 bubble-circle">
             <Icons.instagram className="h-12 w-12" />
+            <div className="bg-noisy pointer-events-none absolute inset-0 z-10 rounded-xl md:rounded-2xl lg:rounded-3xl" />
           </div>
-          <div className="bubble_leftRight4 group relative -left-4 flex aspect-square w-20 items-center justify-center overflow-hidden rounded-full border after:absolute after:inset-0 after:rounded-full sm:w-24 md:w-32">
+          <div className="bubble_leftRight2 bubble-circle">
             <Icons.linkedin className="h-12 w-12" />
+            <div className="bg-noisy pointer-events-none absolute inset-0 z-10 rounded-xl md:rounded-2xl lg:rounded-3xl" />
           </div>
-          <div className="bubble_leftRight group relative -left-4 flex aspect-square w-20 items-center justify-center overflow-hidden rounded-full border after:absolute after:inset-0 after:rounded-full sm:w-24 md:w-32">
+        </motion.div>
+        <motion.div
+          className="z-10 flex min-w-full flex-[0_0_auto] flex-col items-center"
+          initial={{ y: 490 * 2 }}
+          animate={{ y: 530 * -2 }}
+          transition={{ repeat: Infinity, duration: 60 }}
+        >
+          <div className="bubble_leftRight3 bubble-circle">
             <Icons.youtube className="h-12 w-12" />
+            <div className="bg-noisy pointer-events-none absolute inset-0 z-10 rounded-xl md:rounded-2xl lg:rounded-3xl" />
           </div>
-          <div className="bubble_leftRight2 group relative -left-4 flex aspect-square w-20 items-center justify-center overflow-hidden rounded-full border after:absolute after:inset-0 after:rounded-full sm:w-24 md:w-32">
+          <div className="bubble_leftRight bubble-circle">
             <Icons.google className="h-12 w-12" />
+            <div className="bg-noisy pointer-events-none absolute inset-0 z-10 rounded-xl md:rounded-2xl lg:rounded-3xl" />
           </div>
-          <div className="bubble_leftRight3 group relative -left-4 flex aspect-square w-20 items-center justify-center overflow-hidden rounded-full border after:absolute after:inset-0 after:rounded-full sm:w-24 md:w-32">
+          <div className="bubble_leftRight2 bubble-circle">
             <Icons.googleAds className="h-12 w-12" />
+            <div className="bg-noisy pointer-events-none absolute inset-0 z-10 rounded-xl md:rounded-2xl lg:rounded-3xl" />
           </div>
-          <div className="bubble_leftRight4 group relative -left-4 flex aspect-square w-20 items-center justify-center overflow-hidden rounded-full border after:absolute after:inset-0 after:rounded-full sm:w-24 md:w-32">
+          <div className="bubble_leftRight bubble-circle">
             <Icons.wise className="h-12 w-12" />
+            <div className="bg-noisy pointer-events-none absolute inset-0 z-10 rounded-xl md:rounded-2xl lg:rounded-3xl" />
           </div>
-        </div>
+        </motion.div>
       </div>
+      <div className="absolute -bottom-10 z-20 h-20 w-full bg-background blur-lg "></div>
+      <div className="absolute inset-0 -top-10 z-20 h-20 w-full bg-background blur-lg "></div>
     </div>
   );
 }
